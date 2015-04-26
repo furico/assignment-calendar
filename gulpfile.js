d7abc5732bcd0e16062c8f2d16c68ec11ffe1d2f
Hiroyuki Furihata <furihatah@gmail.com>
2015-04-26 11:24:22 +0900

    css-modalを導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 'use strict'
 var gulp = require('gulp');
-var sass = require("gulp-sass");
+var sass = require('gulp-sass');
+var del = require('del');
+
+var config = {
+  sassPath: './assets/scss',
+  bowerDir: './bower_components',
+  staticCssDir: './static/css',
+}
 
 gulp.task('sass', function() {
-    gulp.src("assets/scss/**/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest('./static/css'));
+  gulp.src(config.sassPath + '/**/*.scss')
+      .pipe(sass({
+        includePaths: [config.bowerDir]
+      }))
+      .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('clean', function(cb) {
+  del([config.staticCssDir + '/**/*.css'], cb);
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch('assets/scss/**/*.scss', ['sass']);
+  gulp.watch(config.sassPath + '/**/*.scss', ['sass']);
 });
